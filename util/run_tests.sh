f5bf77080eb6d328baf5d57d76cb67cc1108d2a6
Bryan Tan <techniux@gmail.com>
2019-04-13 19:10:52 -0700

    Fix download tests (attempt 1)

--- util/run_tests.sh
+++ util/run_tests.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+set -x -e
 
-TEST_DIR=""
+# Start http server
+nginx_cont="nginx"
+docker create -p 8080:80 --name $nginx_cont nginx:alpine
+docker cp site-ci.conf $nginx_cont:/etc/nginx/conf.d/default.conf
+docker cp serve/. $nginx_cont:/usr/share/nginx/html
+docker start $nginx_cont
 
-echo "Hello world!"
+# Setup tempget
+app_cont="tempget_app"
+docker create --name $app_cont --network container:$nginx_cont debian:stretch /bin/sh -c 'while true; do sleep 1; done'
+docker cp ../target/debug/tempget $app_cont:/usr/bin/tempget
+docker cp test_templates $app_cont:/test_templates
+docker start $app_cont
+docker exec $app_cont mkdir /testing
+
+# Execute tests
+for f in $(ls test_templates); do
+    docker exec -w /testing $app_cont tempget "/test_templates/$f"
+done
