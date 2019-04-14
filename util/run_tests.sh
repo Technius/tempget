f09dcdbead68d247ac72d31708942a7103a2eeff
Bryan Tan <techniux@gmail.com>
2019-04-13 23:52:32 -0700

    Fix download tests (attempt 10)

--- util/run_tests.sh
+++ util/run_tests.sh
@@ -3,7 +3,7 @@ set -x -e
 
 # Start http server
 nginx_cont="nginx"
-docker create -p 8080:80 --name $nginx_cont nginx:alpine
+docker create --name $nginx_cont nginx:alpine
 docker cp site-ci.conf $nginx_cont:/etc/nginx/conf.d/default.conf
 docker cp serve/. $nginx_cont:/usr/share/nginx/html
 docker start $nginx_cont
@@ -14,5 +14,6 @@ testimage="tempget_test"
 
 # Execute tests
 for f in $(ls test_templates); do
-    docker run --network container:$nginx_cont --rm $testimage "/test_templates/$f"
+    docker run --network container:$nginx_cont --rm $testimage \
+           -c timeout 45 tempget "/test_templates/$f"
 done
