1e0a7c66bb800fd84d25fddb1a40bd903b196179
Bryan Tan <techniux@gmail.com>
2019-04-16 12:29:49 -0700

    Make template file argument required

--- src/cli.rs
+++ src/cli.rs
@@ -11,7 +11,7 @@ use errors;
 #[derive(StructOpt, Debug, Clone)]
 #[structopt(name = "tempget", about = "Downloads files based on a template")]
 pub struct CliOptions {
-    #[structopt(default_value = "template.toml", parse(from_os_str))]
+    #[structopt(parse(from_os_str))]
     /// The template file to use. By default, this is set to "template.toml".
     pub template_file: PathBuf,
     #[structopt(long = "no-extract")]
