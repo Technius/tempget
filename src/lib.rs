ef1091dcd79df7a2da628d6a5e0122a875dc4b95
TA Thanh Dinh <tathanhdinh@gmail.com>
2019-04-24 16:15:34 +0200

    add Error type for failed to download

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,21 @@ pub mod errors {
         pub code: ::reqwest::StatusCode
     }
 
+    #[derive(Fail, Debug)]
+    /// Files which have failed to download
+    pub struct DownloadsFailed {
+        pub files: Vec<(std::path::PathBuf, String)>
+    }
+
+    impl std::fmt::Display for DownloadsFailed {
+        fn fmt(&self, ft: &mut std::fmt::Formatter) -> std::fmt::Result {
+            let msgs = self.files.iter()
+                .map(|(f, err)| format!("\t{}: {}", f.display(), err))
+                .collect::<Vec<String>>();
+            write!(ft, "the following downloads failed:\n{}", msgs.join("\n"))
+        }
+    }
+
     /// Constructs a `Timeout` error
     pub fn timeout(seconds: u64) -> Error {
         Timeout(seconds).into()
@@ -29,5 +44,10 @@ pub mod errors {
     /// Constructs a `StatusCode` error
     pub fn status_code(code: ::reqwest::StatusCode) -> Error {
         (StatusCode { code }).into()
+    }
+
+    /// Constructs a `DownloadsFailed` error
+    pub fn download_failed(files: Vec<(std::path::PathBuf, String)>) -> Error {
+        DownloadsFailed { files }.into()
     }
 }
